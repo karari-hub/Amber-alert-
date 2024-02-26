caaa7919af89761c7122fdc40ad0813273b244ad
brian karari <bkarari1998@gmail.com>
2024-02-27 02:50:47 +0300

    fixed database errors and bugs deleted by the previus dummy data added serch by email and registerd user model yet to change the models further

--- amber_alert_api/settings.py
+++ amber_alert_api/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 ]
 
 
-# AUTH_USER_MODEL = ''
+# AUTH_USER_MODEL = 'base.models.user'
 
 
 MIDDLEWARE = [
--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Users, ChildInformation, MissingPersons,Reports,Alerts,AlertRecipients,Messages
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+
+
+#serch by email function
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+    class Meta:
+        model= User
+
+admin.site.register(User, UserAdmin)
+
 # Register your models here.
 admin.site.register(Users)
 admin.site.register(ChildInformation)
